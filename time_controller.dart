b49ead1f95eb3667ebf62c8e9a4ecb92946e86de
jysong0101 <juyeop0101@khu.ac.kr>
2024-11-09 23:10:13 +0900

    folder stucture change & add client & add server

--- time_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:shelf/shelf.dart';
-
-Future<Response> handleTimeRequest(Request request) async {
-  // 수정
-  final currentTime = DateTime.now().toUtc().toIso8601String();
-  return Response.ok('Current UTC time is: $currentTime');
-}
